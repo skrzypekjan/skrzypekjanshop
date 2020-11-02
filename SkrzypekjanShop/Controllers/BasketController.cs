8158dab8e5f0246501971960681a39f1f3b4dc60
Jan <js.skrzypkowski@gmail.com>
2020-11-02 20:24:58 +0100

    New controller to showing selected subjects

---
+++ SkrzypekjanShop/Controllers/BasketController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SkrzypekjanShop.Models;
+
+namespace SkrzypekjanShop.Controllers
+{
+    public class BasketController : Controller
+    {
+
+        private static List<Product> basket = new List<Product>();
+        List<Product> products = ProductController.products;
+
+        // GET: Basket
+        public ActionResult Index()
+        {
+            return View(basket);
+        }
+
+        // GET: Basket/Add
+        public ActionResult Add(int id)
+        {
+            try
+            {
+                basket.Add(products.FirstOrDefault(x => x.ProductId == id));
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(basket);
+            }
+        }
+
+        // GET: BasketController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                basket.Remove(products.FirstOrDefault(x => x.ProductId == id));
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(basket);
+            }
+        }
+    }
+}
